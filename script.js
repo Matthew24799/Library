06cd37981ff5788b02049e9f198429becdb08d24
Matthew24799 <157596330+Matthew24799@users.noreply.github.com>
2024-07-24 19:06:07 -0400

    stylized card, changed html a lil, working on Card Function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+const container = document.getElementById("container")
 
 const myLibrary = [];
 
@@ -16,29 +16,37 @@ function addBookToLibrary() {
 };
 
 
-function collection() {
+function makeCard() {
     
 
     for (i in myLibrary) {
         const card = document.createElement("div");
         card.classList.add("card");
-        const title = document.createElement("div");
 
-        const bookTitle = document.createTextNode(myLibrary[i].title);
+        const title = document.createElement("p");
+        const author = document.createElement("p");
+        const pages = document.createElement("p");
+
+
+        title.innerText = `Title: ${myLibrary[i].title}`;
+        author.innerText = `Author: ${myLibrary[i].author}`;
+        pages.innerText = `Pages: ${myLibrary[i].pages}`;
 
         console.log(myLibrary[i])
         console.log(myLibrary[i].title);
         
      
 
-        title.appendChild(bookTitle); 
-        document.body.appendChild(card)
-        document.body.lastChild.appendChild(title)
         
+        container.appendChild(card);
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(pages);
     }
 }
 
-const book1 = new book("toad","ahron", "100", "yes");
-myLibrary.push(book1);
+const book1 = new book("Game Of Thrones","ahron", "100", "yes");
+const book2 = new book("Skyward","Ralpth", "200", "no");
+myLibrary.push(book1, book2);
 
-collection();
+makeCard();
