02fc7f68063f4416db9fae0062e09fbc02534856
Matthew24799 <157596330+Matthew24799@users.noreply.github.com>
2024-07-24 06:22:00 -0400

    making Cards to display the books

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
+
+
 const myLibrary = [];
 
-function book(title) {
-    this.title = title;
+function book(title, author, pages, read) {
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.read = read;
 
 };
 
 function addBookToLibrary() {
-    const addBook = "skyward";
-    
-    const newBook = new book(addBook) 
-    myLibrary.push(newBook)
-    console.log(newBook);
+  
+
 };
 
 
 function collection() {
+    
+
     for (i in myLibrary) {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        const title = document.createElement("div");
+
+        const bookTitle = document.createTextNode(myLibrary[i].title);
+
         console.log(myLibrary[i])
+        console.log(myLibrary[i].title);
+        
+     
+
+        title.appendChild(bookTitle); 
+        document.body.appendChild(card)
+        document.body.lastChild.appendChild(title)
+        
     }
 }
 
-const book1 = new book("toad");
-const book2 = new book("dog");
-myLibrary.push(book1, book2);
-
-addBookToLibrary();
+const book1 = new book("toad","ahron", "100", "yes");
+myLibrary.push(book1);
 
 collection();
-
-console.log(myLibrary);
