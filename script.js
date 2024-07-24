fd54cdcedc6092b1f38ae81b96b4c481604caa8b
Matthew24799 <157596330+Matthew24799@users.noreply.github.com>
2024-07-24 04:08:07 -0400

    made function to loop through book array

--- script.js
+++ script.js
@@ -9,8 +9,23 @@ function addBookToLibrary() {
     const addBook = prompt("give title");
     
     const newBook = new book(addBook) 
-
+    myLibrary.push(newBook)
     console.log(newBook);
 };
 
+
+function collection() {
+    for (i in myLibrary) {
+        console.log(myLibrary[i])
+    }
+}
+
+const book1 = new book("toad");
+const book2 = new book("dog");
+myLibrary.push(book1, book2);
+
 addBookToLibrary();
+
+collection();
+
+console.log(myLibrary);
